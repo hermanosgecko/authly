aa8a8f57c90c8957b28a82eb506ad74b3a88dba4
hermanosgecko <jtmitch@protonmail.com>
2020-02-26 15:11:31 +1100

    add travis-ci

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # authentik
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+[![Build Status](https://travis-ci.com/hermanosgecko/authentik.svg?branch=master)](https://travis-ci.com/hermanosgecko/authentik)
--- src/main/java/com/github/hermanosgecko/authentik/App.java
+++ src/main/java/com/github/hermanosgecko/authentik/App.java
@@ -38,56 +38,56 @@ import com.github.hermanosgecko.authentik.token.TokenUtils;
 import com.github.hermanosgecko.authentik.user.UserService;
 
 public class App {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
-	
+
 	private static final String COOKIE_NAME = "authentik.token";
 
 	private static final UserService userService = new UserService();
-	
+
 	private static String cookieDomain;
 	private static String authHost;
 	private static int validityPeriod;
 	private static String pwdFile;
 	private static String secret;
-	
+
 	public static void main(String[] args) {
-			
+
 		LOGGER.info("Starting");
 
 		// create command line options
 		final Options options = new Options()
 				.addOption(Option.builder("c")
-					.desc("cookie domain")
-					.hasArg()
-					.required()
-					.type(String.class)
-					.build())
+						.desc("cookie domain")
+						.hasArg()
+						.required()
+						.type(String.class)
+						.build())
 				.addOption(Option.builder("h")
-					.desc("authentication hostname")
-					.hasArg()
-					.required()
-					.type(String.class)
-					.build())
+						.desc("authentication hostname")
+						.hasArg()
+						.required()
+						.type(String.class)
+						.build())
 				.addOption(Option.builder("p")
-					.desc("validity period")
-					.hasArg()
-					.required()
-					.type(Integer.class)
-					.build())
+						.desc("validity period")
+						.hasArg()
+						.required()
+						.type(Integer.class)
+						.build())
 				.addOption(Option.builder("f")
-					.desc("htpasswd file")
-					.hasArg()
-					.required()
-					.type(String.class)
-					.build())
+						.desc("htpasswd file")
+						.hasArg()
+						.required()
+						.type(String.class)
+						.build())
 				.addOption(Option.builder("s")
-					.desc("token secret")
-					.hasArg()
-					.required()
-					.type(String.class)
-					.build());
-		
+						.desc("token secret")
+						.hasArg()
+						.required()
+						.type(String.class)
+						.build());
+
 		// parse the command line arguments
 		CommandLineParser parser = new DefaultParser();
 		CommandLine cmd;
@@ -97,17 +97,17 @@ public class App {
 			LOGGER.error(e.getLocalizedMessage());
 			return;			
 		}
-		  catch (ParseException e) {
+		catch (ParseException e) {
 			LOGGER.error("Unexpected error", e);
 			return;
 		}
-		
+
 		cookieDomain = cmd.getOptionValue("c");
 		LOGGER.info(MessageFormat.format("cookie domain set to {0}", cookieDomain ));
-		
+
 		authHost = cmd.getOptionValue("h");
 		LOGGER.info(MessageFormat.format("auth host set to {0}", authHost ));
-		
+
 		try {
 			validityPeriod = Integer.parseInt(cmd.getOptionValue("p"));
 			LOGGER.info(MessageFormat.format("validity period set to {0}", validityPeriod ));
@@ -123,38 +123,38 @@ public class App {
 		LOGGER.info(MessageFormat.format("token secret set to {0}", secret ));
 
 		userService.setFileName(pwdFile);
-				
+
 		staticFiles.location("/static");
-		
+
 		notFound((request, response) -> {
 			response.redirect("/auth", 301);
 			return "";
 		});
-		
+
 		get("/auth", (request, response) -> {
-						
+
 			final String fwd_host = request.headers("X-Forwarded-Host");
 			final String fwd_uri = request.headers("X-Forwarded-Uri");
 			final String fwd_method = request.headers("X-Forwarded-Method");
-			
+
 			if(fwd_host == null || fwd_uri == null || fwd_method == null) {
 				response.status(503);
 				return response;
 			}
-			
+
 			if(fwd_host.equals(authHost)) {
 				response.status(200);
 				return response;
 			}
-			
+
 			final String token = request.cookie(COOKIE_NAME);
 			try {
-				
+
 				String user = TokenUtils.validate(secret, cookieDomain, token);
-	        	response.header("X-Forwarded-User", user);
+				response.header("X-Forwarded-User", user);
 				response.status(200);
 				return response;
-				
+
 			}catch(TokenException ex) {		      	
 				response.removeCookie(COOKIE_NAME);
 				response.redirect(MessageFormat.format("http://{0}/?redirect_uri={1}", authHost, Utils.buildRedirectUri(request)),307);
@@ -164,32 +164,32 @@ public class App {
 		});
 
 		post("/login", (request, response) -> {
-			      	
-        	final String redirect_uri = request.queryParams("redirect_uri");
-        	final String username = request.queryParams("username");
-            final String password = request.queryParams("password");
-        	
-            if(redirect_uri == null ||  username == null || password == null) {
-        		response.status(503);
-        		return response;
-        	}
-            
-            if(userService.authenticate(username, password)) {
-    			response.removeCookie(COOKIE_NAME);
-				
-    			String token = TokenUtils.create(secret, cookieDomain, username, Instant.now().plusSeconds(validityPeriod).toEpochMilli());          	
-            	response.cookie(cookieDomain, "/", COOKIE_NAME, token, validityPeriod, false, false);
-            	response.header("X-Forwarded-User", username);
-            	
-            	response.redirect(redirect_uri);
-            	return response;
-            }
-            
-            // if not authenticated return error
-            response.status(401);
-            return response;
+
+			final String redirect_uri = request.queryParams("redirect_uri");
+			final String username = request.queryParams("username");
+			final String password = request.queryParams("password");
+
+			if(redirect_uri == null ||  username == null || password == null) {
+				response.status(503);
+				return response;
+			}
+
+			if(userService.authenticate(username, password)) {
+				response.removeCookie(COOKIE_NAME);
+
+				String token = TokenUtils.create(secret, cookieDomain, username, Instant.now().plusSeconds(validityPeriod).toEpochMilli());          	
+				response.cookie(cookieDomain, "/", COOKIE_NAME, token, validityPeriod, false, false);
+				response.header("X-Forwarded-User", username);
+
+				response.redirect(redirect_uri);
+				return response;
+			}
+
+			// if not authenticated return error
+			response.status(401);
+			return response;
 		});
-			
+
 	}
-	
+
 }
